7a650ab6e8940bafe23f6deea70bc425bb3af502
sultan901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2023-05-17 13:05:22 +0300

    return to main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("What u are u doing")
   return (
     <div className="App">
       <header className="App-header">
