ddc8009ac423d338572909dc10fff83b2a3f5533
Sultan <s.alharbi@thegarage.sa>
2024-07-24 12:40:29 +0300

    fix something

---
+++ test.js
@@ -0,0 +1,6 @@
+function removeUrlAnchor(url) {
+  let split = url.split("").indexOf(".");
+  console.log(split);
+    // return;
+}
+console.log(removeUrlAnchor("'www.codewars.com/katas/?page=1#about'"));
